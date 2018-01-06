a681e0b811476bf6df29f9ac825229fae4e9de3e
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2018-01-06 11:55:00 -0500

    test

--- variables.js
+++ variables.js
@@ -10,6 +10,6 @@ console.log(personality);
 
 // Your Turn!
 // change the value of the `personality` variable to something that describes you
-// and console.log the new value
+// and console.log the new value.
 
 
