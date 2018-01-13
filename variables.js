e095aff92453561e6e0821e6c80dbb86ff250678
Mario Rodrigues <mariorodjr92@gmail.com>
2018-01-13 18:52:10 +0000

    Done.

--- variables.js
+++ variables.js
@@ -5,11 +5,9 @@ var personality = "awesome"
 console.log(personality)
 //
 
-personality = "fabulous";
+personality = "Adventurous";
 console.log(personality);
 
 // Your Turn!
 // change the value of the `personality` variable to something that describes you
 // and console.log the new value
-
-
