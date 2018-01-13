e13312d518601f4e5a5599fd30555983700b5dcc
David Mendelovits <david.mendelovits@gmail.com>
2018-01-13 23:36:11 +0000

    Done.

--- variables.js
+++ variables.js
@@ -1,15 +1,13 @@
-name = "Beyonce";
+name = "David";
 console.log(name);
 
-var personality = "awesome"
+var personality = "ambitioous"
 console.log(personality)
 //
 
-personality = "fabulous";
+personality = "still ambitioous";
 console.log(personality);
 
 // Your Turn!
 // change the value of the `personality` variable to something that describes you
 // and console.log the new value
-
-
