ac27cd3fafa136e9a47ac281dfde34eb88aacd08
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 02:07:11 +0000

    Done.

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -1,8 +1,22 @@
+/*
+var livePerformance = (500+750+3000+5100+12980+9087+50000)
+var musicSales = (500+750+3000+5100+12980+9087+50000)
+*/
+
+
+function beyonceEarnings(num1, num2, num3, num4, num5, num6, num7) {
+  return num1 + num2 + num3 + num4 + num5 + num6 + num7
+}
+
+  var totalSales = beyonceEarnings (500,750,3000,5100,12980,9087,50000)
+
+/*I think this is where I now need to learn looping? To have just one num argument?*/
+
 console.log("Dear Beyonce,")
 console.log("We have calculated your total earnings this year.")
-console.log("You have earned " + beyoncesMoney)
-console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from your live performances.")
-console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
+console.log("You have earned " + totalSales +".")
+console.log("Of this " + totalSales + " a portion derives from your live performances and a portion comes directly from your music sales.")
+console.log("Feel free to contact me with any questions.")
 
 
 // DON'T DO THIS PART YET:
