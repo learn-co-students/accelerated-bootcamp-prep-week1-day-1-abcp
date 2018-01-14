9ff4af9af0e6545d4955ad55f78a593e86ac7204
Dylan Ferrara <dylan.ferrara@gmail.com>
2018-01-14 01:46:40 +0000

    Done.

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -9,5 +9,6 @@ console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some porti
 // Now that we understand what a variable is
 // rewrite the above lines of code using a variable to store the
 // total sum, so that you don't have to add up all those numbers three times
-
+var name1 = "Beyonce";
+var totalSum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000;
 
