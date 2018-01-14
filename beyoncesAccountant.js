88d093f6ae1c3ac004f3895e8b06d2b2ecaf335c
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-14 01:06:35 +0000

    Done.

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -1,13 +1,13 @@
+
+var totalValue = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000;
 console.log("Dear Beyonce,")
 console.log("We have calculated your total earnings this year.")
-console.log("You have earned " + beyoncesMoney)
-console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from your live performances.")
-console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
+console.log("You have earned "+ totalValue)
+console.log("Of " + totalValue + " some portion derives from your live performances.")
+console.log("Of " + totalValue+ " some portion derives from music sales.")
 
 
 // DON'T DO THIS PART YET:
 // Now that we understand what a variable is
 // rewrite the above lines of code using a variable to store the
 // total sum, so that you don't have to add up all those numbers three times
-
-
