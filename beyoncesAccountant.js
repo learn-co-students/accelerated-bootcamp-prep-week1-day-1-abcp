af3062b9da493246c20a85e6d1fe1f483ffb0857
Novera Lim <lim.novera88@gmail.com>
2018-01-15 22:11:01 +0000

    Done.

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -5,6 +5,7 @@ console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some porti
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
 
 
+
 // DON'T DO THIS PART YET:
 // Now that we understand what a variable is
 // rewrite the above lines of code using a variable to store the
