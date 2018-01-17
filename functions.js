e80782d13ae67ba1ff9a7928e0cde731acfbf133
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-17 23:27:53 +0000

    Done.

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 function tellJoke() {
-  console.log("How do you make 'Seven' even?\nYou take away the 'S'")
+  console.log("How do you make 'Seven' even?")
+  console.log("You take away the 'S'")
 }
 
 tellJoke();
@@ -10,9 +11,8 @@ function NameofFunction() {
 }
 
 function addNumbers(num1, num2, num3, num4) {
-  num1 + num2 + num3 + num4
+return  num1 + num2 + num3 + num4
 }
 
 
 addNumbers()
-
--- variables.js
+++ variables.js
@@ -7,9 +7,8 @@ console.log(personality)
 
 personality = "fabulous";
 console.log(personality);
-
+personality = "cool";
+console.log(personality);
 // Your Turn!
 // change the value of the `personality` variable to something that describes you
 // and console.log the new value
-
-
